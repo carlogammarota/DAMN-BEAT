7be88cebc5dab7862c9de2d513cfbcbd8d9b749b
Carlo Gammarota <carlo.gammarota@lemondata.com.ar>
2019-07-01 16:41:25 -0300

    error a arreglar en UPLOAD

--- components/Menubar.vue
+++ components/Menubar.vue
@@ -15,7 +15,8 @@
       <p class="my-2">El tamaño del archivo no debe superar los 50mb, los formatos aceptados son MP3 y Wav.</p>
       //- <dropzone id="foo" ref="el" :options="options" @change="processFile($event)" :destroyDropzone="true"></dropzone>
       //- <b-button variant="outline-primary" @click="processFile($event)">Upload</b-button>
-      <input type="file" @change="processFile($event)">
+      //- <input type="file" @change="processFile($event)">
+      <Upload></upload>
       //- .data
       //- <font-awesome-icon icon="fas-AlignLeft" />
       
@@ -34,7 +35,7 @@ import Albums from '~/components/Albums.vue'
 import Author from '~/components/Author.vue'
 import Contacts from '~/components/Contacts.vue'
 import CosmicLogo from '~/components/CosmicLogo.vue'
-
+import Upload from "~/components/upload"
 const axios = require("axios");
 
 export default {
@@ -49,6 +50,7 @@ export default {
   },
   name: 'menubar',
   components: {
+    Upload,
     Albums,
     Author,
     Contacts,
@@ -69,101 +71,101 @@ export default {
     // const instance = this.$refs.el.dropzone
   },
   methods: {
-    async processFile(event) {
-      console.log("event", event)
-      var bodyFormData = new FormData();
-      let media = event.target.files[0];
-      bodyFormData.append("media", media);
+    // async processFile(event) {
+    //   console.log("event", event)
+    //   var bodyFormData = new FormData();
+    //   let media = event.target.files[0];
+    //   bodyFormData.append("media", media);
 
-      let res = await axios({
-        method: "post",
-        url: "https://api.cosmicjs.com/v1/damnvort/media",
-        config: { headers: { "Content-Type": "multipart/form-data" } },
-        data: bodyFormData
-      });
+    //   let res = await axios({
+    //     method: "post",
+    //     url: "https://api.cosmicjs.com/v1/damnvort/media",
+    //     config: { headers: { "Content-Type": "multipart/form-data" } },
+    //     data: bodyFormData
+    //   });
 
-      let data = res.data.media;
-      //   console.log("data", data);
+    //   let data = res.data.media;
+    //   //   console.log("data", data);
 
-      // add object
-      // /v1/:your-bucket-slug/add-object
-      let res2 = await axios({
-        method: "post",
-        url: "https://api.cosmicjs.com/v1/damnvort/add-object",
-        config: { headers: { "Content-Type": "multipart/form-data" } },
-        data: {
-          title: "Nuevo Titulo 1",
-          slug: "new-titulo",
-          type_slug: "tracks",
-          content: "",
-          metafields: [
-            {
-              type: "file",
-              title: "Musica",
-              key: "audio",
-              value: data.name
-            }
-          ]
-        }
-      });
+    //   // add object
+    //   // /v1/:your-bucket-slug/add-object
+    //   let res2 = await axios({
+    //     method: "post",
+    //     url: "https://api.cosmicjs.com/v1/damnvort/add-object",
+    //     config: { headers: { "Content-Type": "multipart/form-data" } },
+    //     data: {
+    //       title: "Nuevo Titulo 1",
+    //       slug: "new-titulo",
+    //       type_slug: "tracks",
+    //       content: "",
+    //       metafields: [
+    //         {
+    //           type: "file",
+    //           title: "Musica",
+    //           key: "audio",
+    //           value: data.name
+    //         }
+    //       ]
+    //     }
+    //   });
 
-      let track = await axios({
-        method: "get",
-        url:
-          "https://api.cosmicjs.com/v1/damnvort/object/new-titulo-a4547010-993e-11e9-9880-2da09d6d4587",
-        config: { headers: { "Content-Type": "multipart/form-data" } }
-      });
-      console.log("beats", track);
-      this.data = track
-      let track2 = track.data.object;
+    //   let track = await axios({
+    //     method: "get",
+    //     url:
+    //       "https://api.cosmicjs.com/v1/damnvort/object/new-titulo-a4547010-993e-11e9-9880-2da09d6d4587",
+    //     config: { headers: { "Content-Type": "multipart/form-data" } }
+    //   });
+    //   console.log("beats", track);
+    //   this.data = track
+    //   let track2 = track.data.object;
 
-      console.log("track2", track2);
-      this.addToBeats(track2);
-      //   console.log("RES2", res2.data.object);
-    },
-    async addToBeats(obj) {
-      //https://api.cosmicjs.com/:bucket_slug/object/:slug
-      let beats = await axios({
-        method: "get",
-        url: "https://api.cosmicjs.com/v1/damnvort/object/walter",
-        config: { headers: { "Content-Type": "multipart/form-data" } }
-      });
-      console.log("beats", beats);
+    //   console.log("track2", track2);
+    //   this.addToBeats(track2);
+    //   //   console.log("RES2", res2.data.object);
+    // },
+    // async addToBeats(obj) {
+    //   //https://api.cosmicjs.com/:bucket_slug/object/:slug
+    //   let beats = await axios({
+    //     method: "get",
+    //     url: "https://api.cosmicjs.com/v1/damnvort/object/walter",
+    //     config: { headers: { "Content-Type": "multipart/form-data" } }
+    //   });
+    //   console.log("beats", beats);
 
-      let beat = beats.data.object;
-      //   let metafields = beats.data.object.metafields;
-      //   metafields[1].push(obj);
+    //   let beat = beats.data.object;
+    //   //   let metafields = beats.data.object.metafields;
+    //   //   metafields[1].push(obj);
 
-      //   console.log("metafields 1", beat.metafields[0].objects);
-      console.log("obj", obj)
-      beat.metafields.objects.push(obj);
-      beat.metadata.tracks.value.push(obj);
-      beat.title = "Cambio";
-      console.log("new beat", {
-        title: "nuevo titulo",
-        metafields: beat.metafields
-      });
-      let res = await axios({
-        method: "PUT",
-        url: "https://api.cosmicjs.com/v1/damnvort/edit-object",
-        config: { headers: { "Content-Type": "multipart/form-data" } },
-        data: {
-          slug: beat.slug,
-          title: "nuevo titulo",
-          // metafields: beat.metafields
-          // metafields: beat.metafields
-          // metafields: beat.metafields
-          //  metafields: [{
-          //   key: 'album_id',
-          //   type: 'text',
-          //   value: this.album._id
-          // }],
-          // [{"object_type":"tracks","value":"5b688a165669b934036d93c0","key":"tracks","title":"Tracks","type":"objects","children":null}]
-        }
-      });
+    //   //   console.log("metafields 1", beat.metafields[0].objects);
+    //   console.log("obj", obj)
+    //   beat.metafields.objects.push(obj);
+    //   beat.metadata.tracks.value.push(obj);
+    //   beat.title = "Cambio";
+    //   console.log("new beat", {
+    //     title: "nuevo titulo",
+    //     metafields: beat.metafields
+    //   });
+    //   let res = await axios({
+    //     method: "PUT",
+    //     url: "https://api.cosmicjs.com/v1/damnvort/edit-object",
+    //     config: { headers: { "Content-Type": "multipart/form-data" } },
+    //     data: {
+    //       slug: beat.slug,
+    //       title: "nuevo titulo",
+    //       // metafields: beat.metafields
+    //       // metafields: beat.metafields
+    //       // metafields: beat.metafields
+    //       //  metafields: [{
+    //       //   key: 'album_id',
+    //       //   type: 'text',
+    //       //   value: this.album._id
+    //       // }],
+    //       // [{"object_type":"tracks","value":"5b688a165669b934036d93c0","key":"tracks","title":"Tracks","type":"objects","children":null}]
+    //     }
+    //   });
 
-      console.log("res", res);
-    }
+    //   console.log("res", res);
+    // }
 
     //  sendFile(dataUpload){
 
---
+++ components/upload.vue
@@ -0,0 +1,124 @@
+<template>
+    <div>
+
+        <!-- <b-button @click="modalShow = !modalShow">Open Modal</b-button>
+        <br>
+        <b-modal v-model="modalShow">Hello From Modal!</b-modal> -->
+
+        <!-- <input type="file" :v-model="asd" /> -->
+        <!-- <input type="submit" value="Upload!"  /> -->
+        <!-- <button @click="sendFile(dataUpload)" >ENVIAR</button> -->
+        <h1>UPLOAD</h1>
+        <input type="file" @change="processFile($event)">
+        <span v-if="uploading">Subiendo Archivo</span>
+         <!-- <b-button v-b-modal.modal-1>Launch demo modal</b-button> -->
+
+        <!-- <b-modal id="modal-1" title="BootstrapVue">
+          <p class="my-4">Hello from modal!</p>
+        </b-modal> -->
+
+
+          <!-- <b-alert show variant="success"><a href="#" class="alert-link">Success Alert</a></b-alert>
+          <b-alert show variant="danger"><a href="#" class="alert-link">Danger Alert</a></b-alert> -->
+        <!-- <button @click="sendFile()">send File</button> --> 
+    </div>
+</template>
+
+<script>
+const axios = require("axios");
+import getBucket from '~/lib/cosmic'
+export default {
+  data() {
+    return {
+      //Cambiar este slug para cambiar el album
+      slugAlbum: "extra-clean",
+      someData: "",
+      modalShow: false,
+      uploading: false
+    };
+  },
+  methods: {
+    async processFile(event) {
+      this.uploading = true;
+      const bucket = await getBucket()
+      let media = event.target.files[0];
+      console.log(media)
+      // Subimos el archivo
+        let mediaUploaded = (await bucket.addMedia({
+          media: media,
+          folder: 'beats',
+          metadata: {
+            caption: 'Detalle del Beat',
+            credit: 'Walter G.'
+          }
+        })).media;
+        console.log("Archivo Subido", mediaUploaded)
+        // Creamos el track
+        const params = {
+          title: 'Titulo del Beat 3 ('+media.name+')',
+          type_slug: 'tracks',
+          content: 'Descripcion del beat',
+          metafields: [
+              {
+                type: "file",
+                title: "Musica",
+                key: "audio",
+                value: mediaUploaded.name
+              }
+            ]
+        }
+        let track = (await bucket.addObject(params)).object;    
+        console.log("Track Agregado", track)
+
+        // Agregamos el track al album extra-clean
+        
+          // Primero obtengo el album, para no pisar si ya hay tracks        
+          const albumObject = (await bucket.getObject({slug: this.slugAlbum})).object;
+          console.log("Album antes de actualizar", albumObject);
+          let metafields = albumObject.metafields;
+          if(!metafields) metafields = []
+          let existMetafieldTracks = false;
+          metafields.map(meta => {
+            console.log("buscando", meta)
+            if(meta.key == 'tracks'){
+              existMetafieldTracks = true;
+              console.log("Encontre el meta", meta)
+              // Encontre el metafield de los tracks, agreamos el que acabamos de subir.
+              let values = meta.value.split(",");
+              values.push(track._id);
+              meta.value = values.join(",");
+              console.log("Nuevo valor de Metafield", meta.value)
+              return meta;
+            }
+            return meta;
+          })
+
+          if(!existMetafieldTracks){
+            console.log("No existe metafield")
+            // No existe o esta vacio, lo agregamos
+            let trackMetafield = 
+              {
+                title: 'Tracks',
+                key: 'tracks',
+                type: 'objects',
+                object_type: 'tracks',
+                value: track._id
+              };
+            metafields.push(trackMetafield)
+          }
+          let album = await bucket.editObject({
+            slug: this.slugAlbum,
+            metafields: metafields
+          })
+        console.log("Album Actualizado", album)
+        this.uploading = false;
+      
+    }
+  }
+};
+</script>  
+<style lang="scss">
+/* // Then include the following */
+@import "bootstrap/scss/bootstrap.scss";
+@import "bootstrap-vue/src/index.scss";
+</style>
